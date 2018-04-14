61c226de0146d39cbd914368cd5f40675eb6d32d
movsb <anhbk@qq.com>
2018-04-15 03:33:03 +0800

    update ppu

--- ppu.go
+++ ppu.go
@@ -4,29 +4,87 @@ import (
 	"image"
 )
 
+// PPU 控制寄存器 $2000
 type PPUCTRL struct {
-	flagNameTable       byte // 0: $2000, 1: $2400, 2: $2800, 3: $2C00
-	flagIncrement       byte // 0: +1, 1: +32
-	flagSpriteTable     byte // 0: $0000, 1: $1000
-	flagBackgroundTable byte // 0: $0000, 1: $1000
-	flagSpriteSize      byte // 0: 8x8, 1: 8x16
-	flagVBlank          byte // 1: 在 vblank 时中断
+	ctrlNameTable       byte // 命名表基地址 0: $2000, 1: $2400, 2: $2800, 3: $2C00
+	ctrlIncrement       byte // 在 CPU 读写 PPUDATA 后的 VRAM 地址增量： 0: +1, 1: +32
+	ctrlSpriteTable     byte // 8x8 精灵图案表地址 0: $0000, 1: $1000；8x16 时忽略
+	ctrlBackgroundTable byte // 背景图案表地址 0: $0000, 1: $1000
+	ctrlSpriteSize      byte // 精灵大小 0: 8x8, 1: 8x16
+	ctrlMasterSlave     byte // 主/从模式选择
+	ctrlEnableNMI       byte // 使能 VBlank NMI 中断 0: 关闭，1: 打开
 }
 
-type PPUMASK struct {
-	flagGrayscale          byte // 0: color, 1: grayscale
-	flagShowLeftBackground byte // 0: hide, 1: show
-	flagShowLeftSprites    byte // 0: hide, 1: show
-	flagShowBackground     byte // 0:hide, 1: show
-	flagShowSprites        byte // 0: hide, 1: show
+func (o *PPUCTRL) Set(v byte) {
+	o.ctrlNameTable = v >> 0 & 3
+	o.ctrlIncrement = v >> 2 & 1
+	o.ctrlSpriteTable = v >> 3 & 1
+	o.ctrlBackgroundTable = v >> 4 & 1
+	o.ctrlSpriteSize = v >> 5 & 1
+	o.ctrlMasterSlave = v >> 6 & 1
+	o.ctrlEnableNMI = v >> 7 & 1
 }
+
+// PPU 掩码寄存器 $2001
+type PPUMASK struct {
+	maskGrayscale          byte // 灰阶图案 0: 彩色，1: 灰阶
+	maskShowLeftBackground byte // 是否显示最左边的8个像素：1: 显示，0: 隐藏
+	maskShowLeftSprites    byte // 是否显示最左边的8个像素内的精灵：1: 显示，0: 隐藏
+	maskShowBackground     byte // 是否显示背景 1: 显示
+	maskShowSprites        byte // 是否显示精灵 1: 显示
+	maskEmphasizeRed       byte
+	maskEmphasizeGreen     byte
+	maskEmphasizeBlue      byte
+}
+
+func (o *PPUMASK) Set(v byte) {
+	o.maskGrayscale = v >> 0 & 1
+	o.maskShowLeftBackground = v >> 1 & 1
+	o.maskShowLeftSprites = v >> 2 & 1
+	o.maskShowBackground = v >> 3 & 1
+	o.maskShowSprites = v >> 4 & 1
+	o.maskEmphasizeRed = v >> 5 & 1
+	o.maskEmphasizeGreen = v >> 6 & 1
+	o.maskEmphasizeBlue = v >> 7 & 1
+}
+
+// PPU 状态寄存器 $2002
+type PPUSTAT struct {
+	statSpriteOverflow byte // 精灵数量溢出
+	statSpriteHit      byte // 精灵碰撞
+	statVBlank         byte // VBlank 已开始
+}
+
+func (o *PPUSTAT) Set(v byte) {
+	o.statSpriteOverflow = v >> 5 & 1
+	o.statSpriteHit = v >> 6 & 1
+	o.statVBlank = v >> 7 & 1
+}
+
+// 精灵地址 $2003
+type OAMADDR byte
+
+// 精灵数据 $2004
+type OAMDATA byte
+
+// 滚动寄存器 $2005
+type PPUSCRL byte
+
+// PPU 写地址 $2006
+type PPUADDR byte
+
+// PPU 读写数据 $2007
+type PPUDATA byte
+
+// 精灵 DMA 寄存器
+type OAMDMAR byte
 
 type PPU struct {
 	MemoryReadWriter
 	console *Console
 
-	Cycle      int // 0-340
-	Scanline   int //0-261
+	Cycle      int // [0,340]
+	Scanline   int // [0,261]
 	FrameCount uint64
 
 	palette   [32]byte
@@ -35,26 +93,34 @@ type PPU struct {
 	front     *image.RGBA
 	back      *image.RGBA
 
-	// NMI flags
-	nmiOccurred bool
-	nmiOutput   bool
-	nmiPrevious bool
-	nmiDelay    byte
-
+	// 寄存器
 	PPUCTRL
-
 	PPUMASK
+	PPUSTAT
+	OAMADDR
+	OAMDATA
+	PPUSCRL
+	PPUADDR
+	PPUDATA
+	OAMDMAR
 
-	// $2003
-	spriteAddr byte // dma sprites
+	oddFrame bool // 奇偶帧标志
 
-	// $2007 PPUDATA
+	nmiOccurred bool // 中断标志：进入 VBlank
+	// 功能同 ctrlEnableNMI
+	// nmiOutput bool // 开启中断输出
 }
 
 func NewPPU(console *Console) *PPU {
 	ppu := PPU{}
 	ppu.front = image.NewRGBA(image.Rect(0, 0, 256, 240))
 	ppu.back = image.NewRGBA(image.Rect(0, 0, 256, 240))
-	ppu.Reset()
+	ppu.Power()
 	return &ppu
+}
+
+func (o *PPU) Power() {
+	o.PPUCTRL.Set(0x00)
+	o.PPUMASK.Set(0x00)
+	o.PPUSTAT.Set(0xA0)
 }
